e57414b3bf0dbcb6050d1d86351ce86dadccaa6f
Ben <bchadwick94@gmail.com>
2020-08-24 21:09:34 +0100

    tsting out homepage bootstrap 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/', to: 'landing#home', as: 'home'
   mount Spina::Engine => '/web'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'login', to: 'session#new', as: 'login'
